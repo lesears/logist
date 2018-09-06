aceb6e7da0f411ef44d08d9bed2bb8035a42bc70
Matt Trombley <mattrtrombley@gmail.com>
2018-09-06 14:55:13 -0400

    Update with plot file

--- TS_hw#2.R
+++ TS_hw#2.R
@@ -17,6 +17,7 @@ library(expsmooth)
 library(lmtest)
 library(seasonal)
 library(dplyr)
+library(ggplot2)
 
 setwd("/Users/matttrombley/Dropbox/Fall 2018/Fall 1/Time Series/HW2/")
 input.file1 <- "Time_Corrected_Well_Data.csv"
@@ -44,18 +45,10 @@ plot(decomp_stl)
 # Plotting overlay of decomposition with original data
 cntrpt <- mean(df_monthly$x)
 plot(wells, col = "black", main = "Well Depth - Trend/Cycle", xlab = "Year", ylab = "Corrected Measurement", lwd = 2)
-lines(decomp_stl$time.series[,1]+1+cntrpt, col = "red", lwd = 2) # Seasonal plot
+lines(decomp_stl$time.series[,1]+cntrpt, col = "red", lwd = 2) # Seasonal plot
 lines(decomp_stl$time.series[,2], col = "red", lwd = 2) # Trend plot
-lines(decomp_stl$time.series[,3]+1+cntrpt, col = "red", lwd = 2) # Remainder Plot
-lines(decomp_stl$time.series[,1]+decomp_stl$time.series[,2]+decomp_stl$time.series[,3], col = "red", lwd = 2) # Total Plot
-
-# Use ggplot to create nice visualizations here
-#
-#
-#
-#
-#
-###############################################
+lines(decomp_stl$time.series[,3]+cntrpt, col = "red", lwd = 2) # Remainder Plot
+#lines(decomp_stl$time.series[,1]+decomp_stl$time.series[,2]+decomp_stl$time.series[,3], col = "red", lwd = 2) # Total Plot
 
 # Use Holt-Winters additive ES for model creation
 HWES.Well <- hw(wells, seasonal = "additive",h=6)
@@ -84,3 +77,20 @@ error_holt=df_test_ts-holttest$mean
 MAE_holt=mean(abs(error_holt))
 MAPE_holt=mean(abs(error_holt)/abs(df_test_ts))
 round(accuracy(holttest),2)
+
+
+# Use ggplot to create nice visualizations here
+test3 <- as.data.frame(HWES.Well$fitted)
+test3$index <- seq.int(nrow(test3))
+#test2$year=paste('20',substr(test2$Group.1,3,4),sep="")
+#test2$month=substr(test2$Group.1,6,7)
+#
+test2$index <- seq.int(nrow(test2))
+#
+###############################################
+
+ggplot()+
+   geom_line(data = test2, color = "#00AFBB", size = 1,aes(x = index, y = x, group=1)) +
+   geom_line(data = test3, size = 1,aes(x=index, y = x, group = 1)) +
+   labs(x = "Month/Year", y = "Corrected Well Value", title = "Corrected Well Values for Well G-860") +
+   theme(plot.title = element_text(hjust = 0.5)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) #+ scale_x_continuous(labels=c("0" = "Oct. 2007", "25" = "Oct. 2009", "50" = "Nov. 2011", "75" = "Dec. 2013", "100" = "Jan. 2016", "125" = "Feb. 2009"))
